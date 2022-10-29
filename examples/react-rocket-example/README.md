0c3c0cb680200b4bc63d554fed5f59c799c9123a
DrPoppyseed <peaske16180@gmail.com>
2022-10-29 14:00:01 +0900

    Add comments to frontend

--- examples/react-rocket-example/README.md
+++ examples/react-rocket-example/README.md
@@ -1,1 +1,4 @@
 # React frontend, Rocket backend example
+
+A minimum example with Firebase demonstrating how `rocket-firebase-auth`
+can be added to any Rocket backend.
--- examples/react-rocket-example/web/src/App.tsx
+++ examples/react-rocket-example/web/src/App.tsx
@@ -2,6 +2,7 @@ import {getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOu
 import {useEffect, useState} from "react";
 import {initializeApp} from "firebase/app";
 
+// Set up the frontend firebase instance
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_API_KEY,
     authDomain: import.meta.env.VITE_AUTH_DOMAIN,
@@ -13,63 +14,75 @@ const firebaseConfig = {
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
+// We'll be using google authentication in our example, but any authentication method (including custom tokens)
+// should be supported by the backend
 const googleAuthProvider = new GoogleAuthProvider();
 
 const useGoogleOAuth = () => async (): Promise<string | null> => {
     const auth = getAuth(firebaseApp);
     const authnedUser = await signInWithPopup(auth, googleAuthProvider);
 
+    // Early return if we fail to authenticate the user with firebase
     if (!authnedUser) return null
 
+    // Get the authenticated user's firebase Id token that we'll use as our bearer token
     const bearerToken = await authnedUser.user.getIdToken(true);
     return fetch(`${import.meta.env.VITE_BASE_URL}/verify`, {
         method: "GET",
         headers: {
-            'Content-type': 'application/json',
-            Authorization: `bearer ${bearerToken}`
+            // Make a GET request with the `Authorization` header set with our bearer token
+            Authorization: `Bearer ${bearerToken}`
         }
     })
         .then(resp => resp.json())
+        // We only want to return the decoded `uid` in our case
         .then(resp => resp.uid)
 };
 
 const App = () => {
     const googleOAuth = useGoogleOAuth();
+    // The verified uid of the authenticated user
     const [uid, setUid] = useState<string | null>(null)
 
     useEffect(() => {
-        onAuthStateChanged(getAuth(firebaseApp), (authnedUser) => {
-            if (authnedUser) {
-                setUid(authnedUser.uid)
+        // Set the local state for cases where a user is already authenticated
+        onAuthStateChanged(getAuth(firebaseApp), (user) => {
+            if (user) {
+                setUid(user.uid)
             }
         });
     });
 
     const handleSignIn = async () => {
+        // Set the local state to the verified user's uid
         setUid(await googleOAuth())
     }
 
     const handleSignOut = async () => {
         const auth = getAuth(firebaseApp);
+        // Sign out of firebase
         await signOut(auth)
+        // Set the local state back to it's initial state
         setUid(null)
     }
 
-    return <div style={{
-        display: "flex",
-        flexDirection: 'column',
-        alignItems: "center"
-    }}>
-        <p>Current uid: {uid || "<no token>"}</p>
-        <button type='button' onClick={() => handleSignIn()} style={{
-            marginBottom: 20
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: 'column',
+            alignItems: "center"
         }}>
-            Send a firebase token
-        </button>
-        <button type='button' onClick={() => handleSignOut()} disabled={!uid}>
-            Delete your token in local state
-        </button>
-    </div>;
+            <p>Current uid: {uid || "<no token>"}</p>
+            <button type='button' onClick={() => handleSignIn()} style={{
+                marginBottom: 20
+            }}>
+                Send a firebase token
+            </button>
+            <button type='button' onClick={() => handleSignOut()} disabled={!uid}>
+                Delete your token in local state
+            </button>
+        </div>
+    );
 };
 
 export default App;
